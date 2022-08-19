4829c6071924791fece814dd5956f216d888e068
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-08-19 10:14:28 +0900

    for, for in, for of 차이점 테스트

---
+++ 자바스크립트 복습/제어문과 반복문/forinof.js
@@ -0,0 +1,33 @@
+var data=['a','b','c'];
+
+Array.prototype.test=function(){/* */}
+
+//for in
+for(i in data){
+    console.log(data[i]);  //a,b,c,[Function (anonymous)] 
+}
+
+for(i in data){
+    console.log(i); //배열의 인덱스 값 출력
+}
+
+//for
+for(i=0; i<data.length; i++){
+    console.log(data[i]);  //출력 test,a,b,c.  
+}
+
+for(i=0; i<data.length; i++){
+    console.log(i); //출력 0,1,2 배열의 인덱스 값 출력
+}
+
+//for of
+for(i of data){
+    console.log(i); //출력 a,b,c 배열의 value값 출력
+}
+
+//결론 
+//for of : 배열의 value 값을 출력
+//for in : 배열의 index 값을 출력
+
+
+
