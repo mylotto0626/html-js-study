c92543b4ccadc03e2907b8633ee10676ca3f413f
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-09-02 16:52:10 +0900

    자바스크립트 -무한 값 입력 받는 Readline

---
+++ 자바스크립트 복습/문제 모음/readline2.js
@@ -0,0 +1,16 @@
+const readline=require("readline");
+const rl=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input=[];
+
+rl.on("line",(line)=>{
+    input.push(line);
+});
+
+rl.on("close",()=>{
+    console.log(input);
+    process.exit();
+});
