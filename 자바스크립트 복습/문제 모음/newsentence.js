5162145ba75ba169f93a6fbdce138b03f19ae4cd
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-09-05 17:12:45 +0900

    자바스크립트 - splice, join, match
    splice - 배열 기존 요소 제거
    join - 배열 요소를 한개의 요소로 합침
    match- 문자열 안에서 특정 문자 찾기

---
+++ 자바스크립트 복습/문제 모음/newsentence.js
@@ -0,0 +1,13 @@
+let words = ['i', 'have', 'a', 'pen', 'i', 'have', 'pineapple', 'i', 'have', 'an', 'apple', 'pen'];
+
+words.splice(4,2); //index 4-5 삭제 . ['i', 'have', 'a', 'pen','pineapple', 'i', 'have', 'an', 'apple', 'pen']
+words.splice(5,3); //index 5-7 삭제 . ['i', 'have', 'a', 'pen','pineapple','apple', 'pen']
+
+let sentence=words.join(' '); //join(). splice 해서 새로 만들어진 배열을 문장으로 합침
+
+let result=sentence.match(/p/g); //join한 배열 전체에서 p 찾기
+
+if(result!=null){
+    console.log(result.length);
+}
+
