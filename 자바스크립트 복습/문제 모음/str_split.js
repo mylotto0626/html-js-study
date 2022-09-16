7f0136a2782b45d5fc36d734c8b2cc870e6c64b6
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-09-16 15:24:00 +0900

    자바스크립트 - 문자열 자르기

---
+++ 자바스크립트 복습/문제 모음/str_split.js
@@ -0,0 +1,11 @@
+//string 객체 함수 split, typeof, substring
+let str='Good morning! Have a nice day.' 
+
+let split=str.split(' ');
+
+console.log(typeof split); //
+
+const one=str.substring(0,4);
+
+console.log(one);
+
