53f5a164f1be6673b9e52c221546e03164d18428
mylotto0626 <65204055+mylotto0626@users.noreply.github.com>
2022-11-28 14:08:48 +0900

    자바스크립트 - 짝수 번째 점심 메뉴 찾기

---
+++ jsreview/문제 모음/findmenu.js
@@ -0,0 +1,18 @@
+let menu = ["hamburger", "pasta", "curry", "chicken", "salad", "tteokbokki", "ramen", "pizza", "gimbap", "sushi"]
+
+let length = menu.length;
+
+console.log(length);
+
+for (let i = 0; i < menu.length; i++) {
+    if (i % 2 == 0) {
+        console.log(menu[i]);
+    }
+}
+
+
+
+
+
+
+
